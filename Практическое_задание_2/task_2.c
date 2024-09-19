6ff9843caeb240268fe6155417626064a4ec5666
Emeteil <vorobievdima43@gmail.com>
2024-09-19 11:19:10 +0700

    if else -> switch case

--- Практическое_задание_2/task_2.c
+++ Практическое_задание_2/task_2.c
@@ -10,13 +10,20 @@ void main() {
     float temp_result;
     char out_char;
 
-    if (temp_char == 'c' || temp_char == 'C')
-        temp_result = (temp * 9/5) + 32, out_char = 'f';
-    else if (temp_char == 'f' || temp_char == 'F')
-        temp_result = (temp - 32) * 5/9, out_char = 'c';
-    else {
-        printf("Output error\n");
-        return;
+    switch (temp_char) {
+        case 'c':
+        case 'C':
+            temp_result = (temp * 9/5) + 32;
+            out_char = 'f';
+            break;
+        case 'f':
+        case 'F':
+            temp_result = (temp - 32) * 5/9;
+            out_char = 'c';
+            break;
+        default:
+            printf("Output error\n");
+            return;
     }
 
     printf("Output temperature: %f%c\n", temp_result, out_char);
