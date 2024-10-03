79374bf38bd958a8bf423656b0277de7e7e4ab1e
Emeteil <vorobievdima43@gmail.com>
2024-10-03 11:08:32 +0700

    add comments

--- Практическое_задание_5/task_5.c
+++ Практическое_задание_5/task_5.c
@@ -6,7 +6,11 @@ void main() {
     printf("a k: ");
     scanf("%d %d", &a, &k);
 
-    int r = (a << k) | (a >> ((sizeof(a) << 3) - k)); // 8 бит
+    int r = (a << k) | (a >> ((sizeof(a) << 3) - k));
+    // sizeof(var_int) => 4
+    // 4 => 0b100
+    // 0b100 << 3 => 0b100000 => 32 бита
+    // int - 4 байта - 32 бита
 
     printf("%d\n", r);
 }
