33763c37c09383576e61fecab793513d9e42eb62
Emeteil <vorobievdima43@gmail.com>
2024-10-10 11:09:02 +0700

    new commit

--- Практическое_задание_6/task_for_4.c
+++ Практическое_задание_6/task_for_4.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
 void main() {
-    int A[16] = {
+    const int length_A = 16;
+    int A[length_A] = {
         9, -8, 7, 6, -5, 4, 3, -2, 1, -2, 3, 4, 5, -6, 7, 8
     };
-    int B[16] = {
+    int B[length_A] = {
         1, 2, -3, 4, -5, 6, 7, -8, 9, 8, 7, 6, 5, -4, -3, 2
     };
 
-    int C[16];
-    for (int i = 0; i < 16; i++)
+    int C[length_A];
+    for (int i = 0; i < length_A; i++)
         C[i] = A[i] + B[i];
     
     printf("Массив С:");
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < length_A; i++)
         printf(" %d", C[i]);
     puts("");
 
     int max_real_A = 0;
-    for (int i = 0; i < 16; i+=2)
+    for (int i = 0; i < length_A; i+=2)
         if (A[i] > max_real_A)
             max_real_A = A[i];
 
     printf("Максимальная реальная часть комплексных чисел массива A: %d\n", max_real_A);
 
     int count_imag_B = 0;
-    for (int i = 1; i < 16; i+=2)
+    for (int i = 1; i < length_A; i+=2)
         if (B[i] < 0)
             count_imag_B++;
 
@@ -33,9 +34,9 @@ void main() {
 
     int sum_C = 0;
     float avg_C = 0;
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < length_A; i++)
         sum_C += C[i];
-    avg_C = sum_C / 16;
+    avg_C = sum_C / length_A;
 
     printf("Cреднее арифметическое среди всех элементов массива C: %f\n", avg_C);
 }
--- Практическое_задание_6/task_for_5_4.c
+++ Практическое_задание_6/task_for_5_4.c
@@ -1,5 +1,84 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+bool is_prime(int x) {
+    if (x == 0 || x == 1) return false;
+    for (int n = 2; n < x; n++)
+        if (x % n == 0) return false;
+    return true;
+}
+
+int max_prime(int M[], int length) {
+    int max = 0;
+    for (int i = 0; i < length; i++)
+        if (is_prime(M[i]) && M[i] > max)
+            max = M[i];
+    return max;
+}
+
+float avg_prime(int M[], int length) {
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+        if (is_prime(M[i])) {
+            sum += M[i];
+            count++;
+        }
+    return (float)sum / count;
+}
+
+int count_odd(int M[], int length) {
+    int count = 0;
+    for (int i = 0; i < length; i++)
+        if (M[i] % 2 != 0)
+            count++;
+    return count;
+}
+
+void print_array(int M[], int length) {
+    for (int i = 0; i < length; i++)
+        printf("%d ", M[i]);
+    puts("");
+}
+
+void random_array_filling(int *M, int length, int max_rand) {
+    for (int i = 0; i < length; i++){
+        *(M + i) = rand() % max_rand;
+    }
+}
 
 void main() {
-    
+    srand(time(NULL));
+    const int length_X = 20;
+    int X[length_X];
+    random_array_filling(X, length_X, 50);
+
+    int new_X_length = length_X + count_odd(X, length_X);
+    int new_X[new_X_length];
+    int max_prime_X = max_prime(X, length_X);
+    for (int i, j = 0; i < length_X; i++) {
+        new_X[i + j] = X[i];
+        if (X[i] % 2 != 0) {
+            j++;
+            new_X[i + j] = max_prime_X;
+        }
+    }
+
+    printf("Массив до вставки: ");
+    print_array(X, length_X);
+
+    printf(
+        "Cреднее  арифметическое простых элементов массива до вставки: %f\n",
+        avg_prime(X, length_X)
+    );
+
+    printf("Массив после вставки: ");
+    print_array(new_X, new_X_length);
+
+    printf(
+        "Cреднее  арифметическое простых элементов массива после вставки: %f\n",
+        avg_prime(new_X, new_X_length)
+    );
 }
