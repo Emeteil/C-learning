96af22870f5b4842df1ecde7bae432055b182654
Emeteil <vorobievdima43@gmail.com>
2024-10-17 11:16:46 +0700

    new commit

--- Практическое_задание_7/task_3.c
+++ Практическое_задание_7/task_3.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+int random_int(int a, int b) {
+	return (rand() % (b - a + 1)) + a;
+}
+
+void rand_str(char str[], size_t size) {
+    for (int i = 0; i < size; i++) {
+        if (
+                random_int(0, 6) == 0
+                &&
+                str[i-1] != ' '
+                &&
+                i != size - 1 && i != 0
+            ) {
+            str[i] = ' ';
+            continue;
+        }
+        str[i] = random_int(97, 122);
+    }
+    str[size] = '\0';
+}
 
 void main() {
-    char str[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    // char str[] = "Loraem ipsum dolaor ayyaaa sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id esat laborum.";
+    
+    srand(time(NULL));
+    size_t len_s = random_int(100, 1000);
+    char str[len_s];
+    rand_str(str, len_s);
+    printf("Строка: %s\n", str);
 
-    char find_char = 'a';
+    const char find_char = 'a';
 
-    const size_t len_str = strlen(str);
+    const size_t len_str = strlen(str) + 1; // 452 + 1
+    // const size_t len_str = sizeof(str) / sizeof(str[0]); // 453
 
     char *max_a_in_word_ptr;
     int max_a_in_word_len = 0;
@@ -16,7 +46,7 @@ void main() {
     int len = 0;
     int a = 0;
     for (int i = 0; i < len_str; i++) {
-        if (str[i] != ' ') {
+        if (str[i] != ' ' && str[i] != '\0') {
             len++;
             if (str[i] == find_char) a++;
         }
@@ -33,6 +63,21 @@ void main() {
     }
 
     char r[max_a_in_word_len];
+
+    // for (int i = 0; i < max_a_in_word_len; i++)
+    //     r[i] = *(max_a_in_word_ptr + i);
     strncpy(r, max_a_in_word_ptr, max_a_in_word_len);
-    printf("%s %d\n", r, max_a);
+    r[max_a_in_word_len] = '\0';
+
+    printf(
+        "Максимальное количество букв %c в слове: %s\n",
+        find_char,
+        r
+    );
+    printf(
+        "Слово состоит из %d букв, %d из которых %c\n",
+        max_a_in_word_len,
+        max_a,
+        find_char
+    );
 }
