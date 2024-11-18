7a28490600027fcfdb651f97e681bc6ec9e9e433
Emeteil <vorobievdima43@gmail.com>
2024-11-18 10:49:36 +0700

    new commit

--- games/snack/header.h
+++ games/snack/header.h
@@ -1,2 +1,4 @@
 void print_game_field(size_t rows, size_t cols, char matrix[rows][cols]);
+void fill_matrix(size_t rows, size_t cols, char matrix[rows][cols]);
+int random_int(int a, int b);
 int kbhit();
