d04c5c13a9ced7bf96b38780f617bc5b333545f0
Emeteil <94739287+Emeteil@users.noreply.github.com>
2025-03-03 15:46:46 +0700

    new commit

---
+++ Практическое_задание_2.2/utils/complex_construction.h
@@ -0,0 +1,10 @@
+#include <singly_linked_int_list.h>
+
+typedef struct three_linked_struct {
+    struct three_linked_struct* forwards;
+    int_snode* up;
+    int_snode* down;
+} three_linked;
+
+three_linked* create_three_linked(three_linked* forwards, int_snode* up, int_snode* down);
+void generate_dot_file_tl(three_linked* head, const char* filename);
